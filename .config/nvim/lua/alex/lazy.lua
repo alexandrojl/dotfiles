e967b0c90a4cc8bd00c58ef852a718a4bba905f6
Alexandro Leon <alexandrollj@gmail.com>
2024-08-12 19:27:29 -0600

    Git integration config

--- .config/nvim/lua/alex/lazy.lua
+++ .config/nvim/lua/alex/lazy.lua
@@ -15,7 +15,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("alex.plugins", {
+require("lazy").setup({ { import = "alex.plugins"},{import = "alex.plugins.lsp"} } ,{
 checker = {
     enabled = true,
     notify = false,
--- .config/nvim/lua/alex/plugins/substitute.lua
+++ .config/nvim/lua/alex/plugins/substitute.lua
@@ -9,6 +9,7 @@ return {
     -- set keymaps
     local keymap = vim.keymap -- for conciseness
 
+
     keymap.set("n", "s", substitute.operator, { desc = "Substitute with motion" })
     keymap.set("n", "ss", substitute.line, { desc = "Substitute line" })
     keymap.set("n", "S", substitute.eol, { desc = "Substitute to end of line" })
